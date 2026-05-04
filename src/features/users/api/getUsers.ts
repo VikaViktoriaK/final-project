81a27fe6c318d88f2ea0b14fafdc098b7daff2ac
viktoria kolosova <vikakolosova04@gmail.com>
2026-05-04 23:39:03 +0300

    chore: format getUsers GraphQL module

---
+++ src/features/users/api/getUsers.ts
@@ -0,0 +1,50 @@
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+import type { UserRow } from "../types";
+
+export const GET_USERS = gql`
+  query Users {
+    users {
+      id
+      email
+      department_name
+      position_name
+      profile {
+        first_name
+        last_name
+        avatar
+      }
+    }
+  }
+`;
+
+type GetUsersResponse = {
+  users: Array<{
+    id: string;
+    email: string;
+    department_name?: string | null;
+    position_name?: string | null;
+    profile?: {
+      first_name?: string | null;
+      last_name?: string | null;
+      avatar?: string | null;
+    } | null;
+  }>;
+};
+
+export function useUsersQuery() {
+  const query = useQuery<GetUsersResponse>(GET_USERS);
+
+  const users: UserRow[] =
+    query.data?.users?.map((u) => ({
+      id: u.id,
+      firstName: u.profile?.first_name ?? "",
+      lastName: u.profile?.last_name ?? "",
+      email: u.email,
+      department: u.department_name ?? "",
+      position: u.position_name ?? "",
+      avatarUrl: u.profile?.avatar ?? undefined,
+    })) ?? [];
+
+  return { ...query, users };
+}
