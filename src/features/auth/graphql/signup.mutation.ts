42db337bd82c91c9d1fd72fa10be85a6cbcd0914
supershmoll <krakasevichcorp@gmail.com>
2026-05-05 16:55:51 +0300

    feat: add registration form flow

---
+++ src/features/auth/graphql/signup.mutation.ts
@@ -0,0 +1,19 @@
+import { gql } from "@apollo/client";
+
+export const SIGNUP_MUTATION = gql`
+  mutation Signup($auth: AuthInput!) {
+    signup(auth: $auth) {
+      access_token
+      refresh_token
+      user {
+        id
+        email
+        role
+        profile {
+          full_name
+          avatar
+        }
+      }
+    }
+  }
+`;
